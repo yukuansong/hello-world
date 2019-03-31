2e77b6584457ff43991c53ccb945010c68d0bc31
maclocal <yukuan.song@uscis.dhs.gov>
2019-03-31 11:36:49 -0400

    cowsay added

--- scripts/rollback.sh
+++ scripts/rollback.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 echo '++++++++++++++++rolling back....+++++++++++++++++++'
 echo "===================================================="
+
+# install cowsay
+sudo apt-get install cowsay
+cowsay "Hello"
